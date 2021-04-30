68fffb46c3aee6be73818586d40cea36179904fd
snidarian <cephalopod31956@gmail.com>
2021-04-30 21:12:57 +0000

    setup sugarpi script

---
+++ setup_sugarpi.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+
+# First you need to install raspi-config or use raspi-config
+# enable the i2c interface in raspi-config
+# sudo raspi-config
+
+
+# run the following script on your pi
+curl http://cdn.pisugar.com/release/Pisugar-power-manager.sh | sudo bash
+
+
+# test if the sugar-pi server is running
+# echo "get battery" | nc -q 0 127.0.0.1 8423
+
+
+
+
+
+
+
+
+
+
+
