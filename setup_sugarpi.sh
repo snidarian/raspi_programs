f300f02caff89f30f6dfd6f9e91265066216203b
snidarian <cephalopod31956@gmail.com>
2021-05-10 15:57:48 -0400

    word

--- setup_sugarpi.sh
+++ setup_sugarpi.sh
@@ -10,6 +10,14 @@
 curl http://cdn.pisugar.com/release/Pisugar-power-manager.sh | sudo bash
 
 
+# by default the sugarpi-server is disables so it must be enabled to start serving battery data
+sudo systemctl enable pisugar-server
+# start the 'pisugar-server' server
+sudo systemctl start pisugar-server
+# Check the status of the daemon/server with this command
+sudo systemctl status pisugar-server
+# The server daemon will also respond to 'stop' and 'disable'
+
 # test if the sugar-pi server is running
 echo "get battery" | nc -q 0 127.0.0.1 8423
 
