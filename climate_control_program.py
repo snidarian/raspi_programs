cac2f35d7fdada9efe2061a75c42b94e3bcd9ed0
snidarian <cephalopod31956@gmail.com>
2021-07-08 23:04:18 +0100

    led status incorporate

--- climate_control_program.py
+++ climate_control_program.py
@@ -2,25 +2,34 @@
 
 
 import Adafruit_DHT as afd
-import RPi as GPIO
+import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 
 DHT_SENSOR = afd.DHT11
-DHT_PIN = 7
+DHT_PIN = 4
 
 while True:
     humidity, temperature = afd.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
         print("Temp = {0:0.1f}C, Humidity = {1:0.1f}%".format(temperature, humidity))
+        GPIO.output(16, GPIO.HIGH)
+        time.sleep(.1)
+        GPIO.output(16, GPIO.LOW)
+        time.sleep(2)
     else:
         print("Sensor Slur (or ill-performed wiring)");
-    time.sleep(1);
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(.5)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(2)
+        
+
 
 
 
