6c067381de3048517e3ea16e34fc9c078f45d868
snidarian <cephalopod31956@gmail.com>
2021-07-09 02:26:59 +0100

    final edit for today

--- climate_control_program.py
+++ climate_control_program.py
@@ -3,13 +3,21 @@
 
 import Adafruit_DHT as afd
 import RPi.GPIO as GPIO
+import threading
 import time
 
+
+# initial all gpio pins
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+# green and red LED status light indicators
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
-
+# Pulse width modulation pins on data out
+R, G, B = 12, 33, 32
+PINS = [R,G,B]
+GPIO.setup(PINS, GPIO.OUT, initial=GPIO.LOW)
+#p = GPIO.PWM(R, )
 
 DHT_SENSOR = afd.DHT11
 DHT_PIN = 4
@@ -29,7 +37,6 @@ while True:
         GPIO.output(18, GPIO.LOW)
         time.sleep(2)
         
-
 
 
 
