2c4bb7fc84cfc7a27413bb6ade48400b8e942a8d
snidarian <cephalopod31956@gmail.com>
2021-07-12 09:24:35 -0400

    functional

--- morse_code_message_generator.py
+++ morse_code_message_generator.py
@@ -42,42 +42,42 @@ def sentence_pause():
 
 # dictionary of lists (Each letter and its beep lengths)
 morse_alphabet = {
-    "a" : [blip(), beep(), letter_pause()],
-    "b" : [beep(), blip(), blip(), blip(), letter_pause()],
-    "c" : [blip(), beep(), blip(), beep(), letter_pause()],
-    "d" : [beep(), blip(), blip(), letter_pause()],
-    "e" : [blip(), letter_pause()],
-    "f" : [blip(), blip(), beep(), blip(), letter_pause()],
-    "g" : [beep(), beep(), blip(), letter_pause()],
-    "h" : [blip(), blip(), blip(), blip(), letter_pause()],
-    "i" : [blip(), blip(), letter_pause()],
-    "j" : [blip(), beep(), beep(), beep(), letter_pause()],
-    "k" : [beep(), blip(), beep(), letter_pause],
-    "l" : [blip(), beep(), blip(), blip(), letter_pause],
-    "m" : [beep(), beep(), letter_pause()],
-    "n" : [beep(), blip(), letter_pause()],
-    "o" : [beep(), beep(), beep(), letter_pause()],
-    "p" : [blip(), beep(), beep(), blip(), letter_pause()],
-    "q" : [beep(), beep(), blip(), beep(), letter_pause()],
-    "r" : [blip(), beep(), blip(), letter_pause()],
-    "s" : [blip(), blip(), blip(), letter_pause()],
-    "t" : [beep(), letter_pause()],
-    "u" : [blip(), blip(), beep(), letter_pause()],
-    "v" : [blip(), blip(), blip(), beep(), letter_pause],
-    "w" : [blip(), beep(), beep(), letter_pause()],
-    "x" : [beep(), blip(), blip(), beep(), letter_pause()],
-    "y" : [beep(), blip(), beep(), beep(), letter_pause()],
-    "z" : [beep(), beep(), blip(), blip(), letter_pause()],
-    "." : [sentence_pause()],
-    " " : [word_pause()]
+    "a" : [blip, beep, letter_pause],
+    # "b" : [beep(), blip(), blip(), blip(), letter_pause()],
+    # "c" : [blip(), beep(), blip(), beep(), letter_pause()],
+    # "d" : [beep(), blip(), blip(), letter_pause()],
+    # "e" : [blip(), letter_pause()],
+    # "f" : [blip(), blip(), beep(), blip(), letter_pause()],
+    # "g" : [beep(), beep(), blip(), letter_pause()],
+    # "h" : [blip(), blip(), blip(), blip(), letter_pause()],
+    # "i" : [blip(), blip(), letter_pause()],
+    # "j" : [blip(), beep(), beep(), beep(), letter_pause()],
+    # "k" : [beep(), blip(), beep(), letter_pause],
+    # "l" : [blip(), beep(), blip(), blip(), letter_pause],
+    # "m" : [beep(), beep(), letter_pause()],
+    # "n" : [beep(), blip(), letter_pause()],
+    # "o" : [beep(), beep(), beep(), letter_pause()],
+    # "p" : [blip(), beep(), beep(), blip(), letter_pause()],
+    # "q" : [beep(), beep(), blip(), beep(), letter_pause()],
+    # "r" : [blip(), beep(), blip(), letter_pause()],
+    # "s" : [blip(), blip(), blip(), letter_pause()],
+    # "t" : [beep(), letter_pause()],
+    # "u" : [blip(), blip(), beep(), letter_pause()],
+    # "v" : [blip(), blip(), blip(), beep(), letter_pause],
+    # "w" : [blip(), beep(), beep(), letter_pause()],
+    # "x" : [beep(), blip(), blip(), beep(), letter_pause()],
+    # "y" : [beep(), blip(), beep(), beep(), letter_pause()],
+    # "z" : [beep(), beep(), blip(), blip(), letter_pause()],
+    # "." : [sentence_pause()],
+    # " " : [word_pause()]
 }
 
 
 def main():
-    text_message = "abc"
+    text_message = "aaa"
     for letter in text_message:
         print(letter)
-        morse_alphabet[letter]()
+        morse_alphabet[letter][0]()
 
 
 
