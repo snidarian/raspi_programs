f844864a22ff3323fca9c2c177d8edaf6b598d2a
snidarian <cephalopod31956@gmail.com>
2021-07-12 08:02:25 -0400

    functioning

--- morse_code_message_generator.py
+++ morse_code_message_generator.py
@@ -11,20 +11,43 @@ GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.OUT)
 
 
+# two functions for blip and beep that make the sound when called
+def blip():
+    GPIO.output(7, GPIO.HIGH)
+    time.sleep(.1)
+    GPIO.output(7, GPIO.LOW)
+
+def beep():
+    GPIO.output(7, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(7, GPIO.LOW)
 
 
-GPIO.output(7, GPIO.HIGH)
-time.sleep(2)
-GPIO.output(7, GPIO.LOW)
-time.sleep(2)
-GPIO.output(7, GPIO.HIGH)
-time.sleep(2)
+# set two global variables 'blip' and 'beep'
+# blip represents dots and beep represents dashes.
+#blip = make_blip()
+#beep = make_beep()
+
+# dictionary of lists (Each letter and its beep lengths)
+morse_alphabet = {
+    "a" : [blip, beep],
+    "b" : [beep, blip, blip, blip],
+    "c" : [blip, beep, blip, beep],
+}
 
 
-# list of lists (Each letter and its beep lengths)
+def main():
+    text_message = input("Type a text message to convert to morse code: ")
+    for letter in text_message:
+        print(morse_alphabet[letter])
 
 
-GPIO.cleanup()
+
+
+
+if __name__ == '__main__':
+    main()
+    GPIO.cleanup()
 
 
 
