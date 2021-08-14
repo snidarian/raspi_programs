7e6a683f0010800fb8e8647c34eca49848a99e81
snidarian <cephalopod31956@gmail.com>
2021-08-14 18:23:56 -0400

    edited file

---
+++ remote_control_leds_with_API.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+
+
+
+import RPi.GPIO as GPIO 
+
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False)
+
+GPIO.setup(17, GPIO.OUT) 
+
+GPIO.setup(18, GPIO.OUT)
+
+GPIO.setup(27, GPIO.OUT)
+
+from flask
+import Flask
+
+app = Flask(__name__)
+
+@ app.route('/') 
+
+def hello_world():
+    return 'Hello, World!'
+
+@
+app.route('/rlon') 
+
+def redledon():
+    GPIO.output(17, GPIO.HIGH)
+    return "Red LED on"
+
+@
+app.route('/rloff') 
+
+def redledoff():
+    GPIO.output(17, GPIO.LOW)
+    return "Red LED off" 
+
+@
+app.route('/glon')
+
+def greenledon():
+    GPIO.output(18, GPIO.HIGH)
+    return "Green LED on"
+
+@
+app.route('/gloff')
+
+def greenledoff():
+    GPIO.output(18, GPIO.LOW)
+    return "Green LED off"
+
+@
+app.route('/blon')
+
+def blueledon():
+    GPIO.output(19, GPIO.HIGH)
+    return "Blue LED on"
+
+@
+app.route('/bloff')
+
+def blueledoff():
+    GPIO.output(19, GPIO.LOW)
+    return "Blue LED off"
+
+@
+app.route('/ylon')
+
+def yellowledon():
+    GPIO.output(19, GPIO.HIGH)
+    return "Yellow LED on"
+
+@
+app.route('/yloff')
+
+def yellowledoff():
+    GPIO.output(19, GPIO.LOW)
+    return "Yellow LED off"
+
+@
+app.route('/wlon')
+
+def whiteledon():
+    GPIO.output(19, GPIO.HIGH)
+    return "White LED on"
+
+@
+app.route('/wloff')
+
+def whiteledoff():
+    GPIO.output(19, GPIO.LOW)
+    return "White LED off"
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
